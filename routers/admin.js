44a5032437cac0992e9be8f524c2a10b3b264559
hanekaoru <vd2o@foxmail.com>
2017-05-13 21:15:32 +0800

    add manage-users

--- routers/admin.js
+++ routers/admin.js
@@ -20,13 +20,50 @@ router.get("/", function (req, res, next) {
 // 用户管理
 router.get("/user", function (req, res, next) {
 
-    // 从数据库中读取所有用户数据
-    User.find().then(function (users) {
-        res.render("admin/user.html", {
-            userinfo: req.userinfo,
-            users: users
+    // 从数据库中读取所有用户数据，需要用到下面几个方法
+
+    // limit(Number) 来获取分页数据
+    // skip() 忽略数据的条数
+
+    // 获取前台发送过来的 page ===>  ?page=1
+    var page = Number(req.query.page || 1);
+
+    // 每页展示几条数据
+    var limit = 2;
+
+    // 总页数
+    var pages = 0;
+
+    // 因为是异步操作，所以需要先查询到数据后在进行渲染
+    User.count().then(function (count) {
+
+        // 限定页数范围
+        pages = Math.ceil(count / limit);
+        // page 取值不能超过 pages（总页数）
+        page = Math.min(page, pages);
+        // page 取值也不能小于 1
+        page = Math.max(page, 1);
+
+         // 获取数据的时候忽略前面多少条
+        var skip = (page - 1) * limit;
+
+        User.find().limit(limit).skip(skip).then(function (users) {
+            res.render("admin/user.html", {
+                userinfo: req.userinfo,
+                users: users,
+
+                // 总条树，每页多少条，共多少页，当前第几页
+                count: count,
+                limit: limit,
+                pages: pages,
+                page: page
+            })
         })
     })
+
+   
+    
+    
 
     
 })
