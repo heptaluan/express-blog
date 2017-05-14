bac0a385526bdd91eb0bf34e3903c9ed15398c99
hanekaoru <vd2o@foxmail.com>
2017-05-14 16:19:37 +0800

    replace templates

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,15 @@
 $(function () {
     
     // 注册
-    $(".btn").on("click", function () {
+    $("#register").on("click", function () {
+        console.log( $("input[name=username]").val() )
         $.ajax({
             url: "/api/user/register",
             type: "post",
             data: {
-                username: $(".user").val(),
-                password: $(".pwd").val(),
-                repassword: $(".repwd").val()
+                username: $("#regsiterBox input[name=username]").val(),
+                password: $("#regsiterBox input[name=password]").val(),
+                repassword: $("#regsiterBox input[name=passwordRepeat]").val()
             },
             dataType: "json",
             success: function (data) {
@@ -17,14 +18,20 @@ $(function () {
         })
     })
 
+    // 去注册页面
+    $("#to-regsiter").on("click", function () {
+        $("#loginBox").hide();
+        $("#regsiterBox").show();
+    })
+
     // 登录
-    $(".btn2").on("click", function () {
+    $("#login").on("click", function () {
         $.ajax({
             url: "/api/user/login",
             type: "post",
             data: {
-                username: $(".user2").val(),
-                password: $(".pwd2").val(),
+                username: $("#loginBox input[name=username]").val(),
+                password: $("#loginBox input[name=password]").val(),
             },
             dataType: "json",
             success: function (data) {
@@ -35,8 +42,16 @@ $(function () {
         })
     })
 
+    // 去登录页面
+    $("#to-login").on("click", function () {
+        $("#regsiterBox").hide();
+        $("#loginBox").show();
+    })
+
+    
+
     // 退出
-    $(".loginout").on("click", function () {
+    $("#logout").on("click", function () {
         $.ajax({
             url: "/api/user/logout",
             success: function (data) {
