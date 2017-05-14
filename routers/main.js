4f801ef4798e75a3528d5590698defddb216de74
hanekaoru <vd2o@foxmail.com>
2017-05-14 20:07:44 +0800

    add inside pages content

--- routers/main.js
+++ routers/main.js
@@ -3,47 +3,82 @@ const router = express();
 const Category = require("../models/Category");
 const Content = require("../models/Content");
 
+var data;
+
+// 处理通用数据
+router.use(function (req, res, next) {
+
+    data = {
+        userinfo: req.userinfo,
+        categories: []
+    }
+    
+    Category.find().then(function (categories) {
+        data.categories = categories;
+        next();
+    })
+
+})
+
 // 前台内容首页
 router.get("/", (req, res, next) => {
 
-    var data = {
-        page: Number(req.query.page || 1),
-        limit: 10,
-        pages: 0,
-        userinfo: req.userinfo,
-        categories: [],
-        count: 0,
-        link: "/"
+    data.page = Number(req.query.page || 1);
+    data.limit = 4;
+    data.pages = 0;
+    data.count = 0;
+    data.category = req.query.category || "";
+
+    var where = {};
+    if (data.category) {
+        where.category = data.category;
     }
     
     // 读取分类信息
-    Category.find().then(function (categories) {
 
-        data.categories = categories
+    Content.count().then(function (count) {
 
-        // 读取内容的总记录数目
-        return Content.count();
+    data.count = count;
 
-    }).then(function (count) {
+    data.pages = Math.ceil(data.count / data.limit);
+    data.page = Math.min(data.page, data.pages);
+    data.page = Math.max(data.page, 1);
+    var skip = (data.page - 1) * data.limit;
 
-        data.count = count;
-
-        data.pages = Math.ceil(data.count / data.limit);
-        data.page = Math.min(data.page, data.pages);
-        data.page = Math.max(data.page, 1);
-        var skip = (data.page - 1) * data.limit;
-
-        // 读取文件列表
-        return Content.find().limit(data.limit).skip(skip).populate(["category", "user"]).sort({ addTime: -1 })
+    // 读取文件列表
+    return Content.where(where).find().limit(data.limit).skip(skip).populate(["category", "user"]).sort({ addTime: -1 })
 
 
     }).then(function (contents) {
 
         data.contents = contents;
 
-        console.log(data)
+        // console.log(data)
         
         res.render("main/index", data)
+    })
+
+})
+
+
+// 查看文章详情
+router.get("/view", function (req, res, next) {
+
+    var contentid = req.query.contentid || "";
+
+    Content.findOne({
+        _id: contentid
+    }).populate("user").then(function (content) {
+
+        data.content = content;
+
+        // 更新阅读数
+        content.views++;
+        content.save();
+
+        
+        // console.log(data)
+        res.render("main/view", data)
     })
 
 })
